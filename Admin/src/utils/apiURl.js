99781cd82f5fafe3ef9fd8f94506abd45df827e0
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-05-15 15:02:47 +0530

    fix

--- Admin/src/utils/apiURl.js
+++ Admin/src/utils/apiURl.js
@@ -1,2 +1,2 @@
 export const API = "https://iiitu-website.onrender.com/api";
-export const STATIC_FILES = "https://iiitu-website.onrender.com/";
+export const STATIC_FILES = "https://iiitu-website.onrender.com";

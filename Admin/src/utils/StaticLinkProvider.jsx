450a818569e585c564df0a92d1f48e1b463e07eb
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-09-13 00:48:59 +0530

    fix: api url

--- Admin/src/utils/StaticLinkProvider.jsx
+++ Admin/src/utils/StaticLinkProvider.jsx
@@ -1,4 +1,4 @@
-import { STATIC_FILES } from "./apiURL";
+import { STATIC_FILES } from "./apiURl";
 
 export const StaticLinkProvider = (link) => {
     return link&&`${STATIC_FILES}/${link.replace(/\\/g, '/')}`;

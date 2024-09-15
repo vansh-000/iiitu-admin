f31bc02bd4e343de6691050fb910d1a2e23293fb
AyushIIITU <ayush01122004@gamil.com>
2024-09-15 17:04:58 +0530

    fix faculty UI

--- Faculty/src/pages/components/PublicationType/Book.jsx
+++ Faculty/src/pages/components/PublicationType/Book.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import DatePickerCustom from '../../../components/DatePickerCoustom';
-const INDEXING = ["SCOPUS", "SCI/SCIE", "ESCI", "WEWOS", "UGC-CARE", "OTHERS"];
+import { FaStarOfLife } from 'react-icons/fa';
+const INDEXING = ['SCOPUS', 'SCI/SCIE', 'ESCI', 'WEWOS', 'UGC-CARE', 'OTHERS'];
 
 function Book({
   refAuthors,
@@ -10,18 +11,17 @@ function Book({
   refTitle,
   refUrl,
   refVol,
-  refOther
+  refOther,
 }) {
   const [selectedIndexing, setSelectedIndexing] = useState('');
   const [otherIndexing, setOtherIndexing] = useState('');
   const handleIndexingChange = (e) => {
     const value = e.target.value;
     setSelectedIndexing(value);
-    if (value !== "OTHERS") {
+    if (value !== 'OTHERS') {
       setIndexing(value);
-    }
-    else{
-      setIndexing(()=>"")
+    } else {
+      setIndexing(() => '');
     }
   };
   const handleOtherIndexingChange = (e) => {
@@ -31,72 +31,77 @@ function Book({
   };
   return (
     <div className="p-4 md:p-5 space-y-4 transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary max-h-[60vh] overflow-y-scroll">
-    <label className="mb-3 contents text-black dark:text-white">
-      Title:
-    </label>
-    <input
-      name="title"
-      type="text"
-      ref={refTitle}
-      placeholder="Title"
-      className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-    />
-    <label className="mb-3 block text-black dark:text-white">
-    Authors(For Multiple Authors use ; to seperate):
-    
-    <input
-      name="title"
-      type="text"
-      ref={refAuthors}
-      placeholder="eg:Rishvant;Anurag;Pundir"
-      className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-    /></label>
-<label className="mb-3 block text-black dark:text-white">
-      Year<DatePickerCustom setDate={setDate}/>
-    
-    {/* <input
+      <label className="mb-3 contents text-black dark:text-white">
+      <FaStarOfLife className='inline text-red-600 text-[10px]'/>Title:</label>
+      <input
+        name="title"
+        type="text"
+        required
+        ref={refTitle}
+        placeholder="Title"
+        className="ml-2 mt-2 w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+      />
+      <label className="mb-3 flex items-center text-black dark:text-white">
+        <div className="block text-xs">
+        <FaStarOfLife className='inline text-red-600 text-[10px]'/> Authors(For Multiple Authors use ; to seperate):
+        </div>
+        <input
+          name="title"
+          required
+          type="text"
+          ref={refAuthors}
+          placeholder="e.g., Rishvant; Singh; Pundir"
+          className="ml-2 mt-2 w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+        />
+      </label>
+      <label className="mb-3 block text-black dark:text-white">
+        Year
+        <DatePickerCustom setDate={setDate} />
+        {/* <input
       name="title"
       type="text"
       ref={refDate}
       placeholder="eg.2021"
       className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
     /> */}
-    </label>
+      </label>
       <label className="mb-3 block text-black dark:text-white">
-        Publisher:
+      <FaStarOfLife className='inline text-red-600 text-[10px]'/> Publisher:
         <input
           name="Publisher"
           type="text"
+          required
           ref={refOther}
           placeholder="Name of Publisher"
           className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
         />
       </label>
-    <label className="mb-3 block text-black dark:text-white">
-      Vol
-    
-    <input
-      name="title"
-      type="text"
-      ref={refVol}
-      placeholder="eg.0.1,1.2,3"
-      className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-    /></label>
-    <label className="mb-3 block text-black dark:text-white">
-      Pages
-    
-    <input
-      name="title"
-      type="text"
-      ref={refPage}
-      placeholder="eg.22-33,24"
-      className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-    /></label>
-          <label className="mb-3 block text-black dark:text-white">
-        Indexing:
+      <label className="mb-3 block text-black dark:text-white">
+        Vol
+        <input
+          name="title"
+          type="text"
+          ref={refVol}
+          placeholder="eg.0.1,1.2,3"
+          className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+        />
+      </label>
+      <label className="mb-3 block text-black dark:text-white">
+        Pages
+        <input
+          name="title"
+          type="text"
+          ref={refPage}
+          placeholder="eg.22-33,24"
+          className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+        />
+      </label>
+      <label className="mb-3 block text-black dark:text-white">
+      <FaStarOfLife className='inline text-red-600 text-[10px]'/>Indexing:
         <select
           value={selectedIndexing}
           onChange={handleIndexingChange}
+          required
           className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-8 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input ${
             selectedIndexing ? 'text-black dark:text-white' : ''
           }`}
@@ -105,13 +110,16 @@ function Book({
             Select Indexing
           </option>
           {INDEXING.map((idx, index) => (
-            <option key={index} value={idx} className="text-body dark:text-bodydark">
+            <option
+              key={index}
+              value={idx}
+              className="text-body dark:text-bodydark"
+            >
               {idx}
             </option>
           ))}
         </select>
-
-        {selectedIndexing === "OTHERS" && (
+        {selectedIndexing === 'OTHERS' && (
           <input
             name="otherIndexing"
             type="text"
@@ -122,18 +130,18 @@ function Book({
           />
         )}
       </label>
-    <label className="mb-3 block text-black dark:text-white">
-      DOI
-    
-    <input
-      name="title"
-      type="text"
-      ref={refUrl}
-      placeholder="eg:https://googleScholer/hkjh"
-      className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-    /></label>
-  </div>
-  )
+      <label className="mb-3 block text-black dark:text-white">
+        DOI
+        <input
+          name="title"
+          type="text"
+          ref={refUrl}
+          placeholder="eg:https://googleScholer/hkjh"
+          className="ml-2 mt-2 w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+        />
+      </label>
+    </div>
+  );
 }
 
 export default Book;
--- Faculty/src/pages/components/PublicationType/Patent.jsx
+++ Faculty/src/pages/components/PublicationType/Patent.jsx
@@ -1,4 +1,5 @@
-import DatePickerCustom from "../../../components/DatePickerCoustom";
+import { FaStarOfLife } from 'react-icons/fa';
+import DatePickerCustom from '../../../components/DatePickerCoustom';
 
 function Patent({
   refAuthors,
@@ -8,34 +9,41 @@ function Patent({
   refTitle,
   refUrl,
   refVol,
-})
-
-{
-
+}) {
   console.log(setDate);
-  
+
   return (
-    <div className="px-5 py-2">
-      <label className="mb-3 contents text-black dark:text-white">Title:</label>
+    <div className="p-4 md:p-5 space-y-4 transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary max-h-[60vh] ">
+      <label className="mb-3 contents text-black dark:text-white">
+        <FaStarOfLife className="inline text-red-600 text-[10px]" />
+        Title:
+      </label>
       <input
         name="title"
+        required
         type="text"
         ref={refTitle}
         placeholder="Title"
         className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
       />
-      <label className="mb-3 block text-black dark:text-white">
-        Inventors(For Multiple Inventors use ; to seperate):
+      <label className="mb-3 flex items-center text-black dark:text-white">
+        <div className="block text-xs">
+          <FaStarOfLife className="inline text-red-600 text-[10px]" />
+          Inventors (For Multiple Inventors use ; to separate):
+        </div>
         <input
           name="title"
+          required
           type="text"
           ref={refAuthors}
-          placeholder="eg:Rishvant;Anurag;Pundir"
-          className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+          placeholder="e.g., Rishvant; Singh; Pundir"
+          className="ml-2 mt-2 w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
         />
       </label>
+
       <label className="mb-3 block text-black dark:text-white">
-        Date Of Grant:<DatePickerCustom setDate={setDate}/>
+        Date Of Grant:
+        <DatePickerCustom setDate={setDate} />
         {/* <input
           name="title"
           type="text"
@@ -45,7 +53,6 @@ function Patent({
         /> */}
       </label>
 
-     
       <label className="mb-3 block text-black dark:text-white">
         DOI
         <input
@@ -53,7 +60,7 @@ function Patent({
           type="text"
           ref={refUrl}
           placeholder="eg:https://googleScholer/hkjh"
-          className="ml-2 mt-2 w-auto rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+          className="ml-2 mt-2 w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
         />
       </label>
     </div>

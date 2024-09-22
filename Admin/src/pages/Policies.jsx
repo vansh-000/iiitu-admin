f7ed5024b1c0128d0ca8bf23f11fbb982d81e3c4
Rishvant Singh <rishvantsinghpundir@gmail.com>
2024-09-22 15:47:45 +0530

    fix: policy and ordinances

--- Admin/src/pages/Policies.jsx
+++ Admin/src/pages/Policies.jsx
@@ -9,10 +9,12 @@ import { jwtDecode } from 'jwt-decode';
 import TablePolicy from '../components/Tables/TablePolicy.jsx';
 
 const TYPE = ['Ordinance', 'Institute Policy'];
+const SECTION = ['UG', 'PG', 'PhD', 'Hostel'];
 
 const Policy = () => {
   const [data, setData] = useState();
   const [selectedType, setSelectedType] = useState('');
+  const [selectedSection, setSelectedSection] = useState('');
   const [isOptionSelected, setIsOptionSelected] = useState(false);
   const token = localStorage.getItem('token');
   const navigate = useNavigate();
@@ -56,6 +58,7 @@ const Policy = () => {
   const handleAdd = async (e) => {
     e.preventDefault();
     const title = titleRef.current.value;
+    const section = selectedSection;
     const type = selectedType;
     try {
       const formData = new FormData();
@@ -63,6 +66,7 @@ const Policy = () => {
         formData.append('Doc', Doc);
       });
       formData.append('title', title);
+      formData.append('section', section);
       formData.append('type', type);
       await axios.post(`${API}/policy`, formData, {
         headers: {
@@ -167,6 +171,65 @@ const Policy = () => {
           </div>
         </div>
 
+        {/* Conditionally render the section dropdown if selectedType is "Ordinance" */}
+        {selectedType === 'Ordinance' && (
+          <div className="mt-4">
+            <label className="mb-3 block text-black dark:text-white">
+              Section
+            </label>
+
+            <div className="relative z-20 bg-white dark:bg-form-input">
+              <select
+                value={selectedSection}
+                required="required"
+                onChange={(e) => {
+                  setSelectedSection(e.target.value);
+                  changeTextColor();
+                }}
+                className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-8 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input ${
+                  isOptionSelected ? 'text-black dark:text-white' : ''
+                }`}
+              >
+                <option
+                  value=""
+                  disabled
+                  className="text-body dark:text-bodydark"
+                >
+                  Select Section
+                </option>
+                {SECTION.map((sec, index) => (
+                  <option
+                    value={sec}
+                    key={index}
+                    className="text-body dark:text-bodydark"
+                  >
+                    {sec}
+                  </option>
+                ))}
+              </select>
+
+              <span className="absolute top-1/2 right-4 z-10 -translate-y-1/2">
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g opacity="0.8">
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                      fill="#637381"
+                    ></path>
+                  </g>
+                </svg>
+              </span>
+            </div>
+          </div>
+        )}
+
         <div className="mt-4">
           <label className="mb-3 block text-black dark:text-white">
             Attach file
@@ -181,13 +244,12 @@ const Policy = () => {
             required
           />
         </div>
-        <button className="inline-flex items-center justify-center rounded-full bg-black mt-2 py-2 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10">
+        <button className="inline-flex mb-4 items-center justify-center rounded-full bg-black mt-2 py-2 px-10 text-center font-medium text-white hover:bg-opacity-90 lg:px-8 xl:px-10">
           Add Policy
         </button>
       </form>
-      <div className="flex flex-col gap-10 mt-5">
-        <TablePolicy data={data} handleDelete={handleDelete} />
-      </div>
+
+      <TablePolicy data={data} handleDelete={handleDelete} />
     </DefaultLayout>
   );
 };
